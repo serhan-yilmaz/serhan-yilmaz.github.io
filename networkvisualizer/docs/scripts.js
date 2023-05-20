2a6cfeb49034311d9c2edb2eead102c2914e84a1
Serhan Yılmaz <serhanyilmz@gmail.com>
2023-05-20 14:38:16 -0400

    Update networkvisualizer docs

---
+++ networkvisualizer/docs/scripts.js
@@ -0,0 +1,5 @@
+<link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/vs.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/languages/matlab.min.js"></script>
+<script>hljs.highlightAll();</script>
